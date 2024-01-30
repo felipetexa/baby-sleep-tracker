bd443b62b841df967a847a35716acfae1ad62040
felipetexa <felipe.teixeira91@hotmail.com>
2024-01-29 19:20:50 -0500

    refactor: using axios for all api calls

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,14 +1,7 @@
 import axios from 'axios';
 
-const config = {
-  headers: {
-    "Access-Control-Allow-Origin": "*",
-    "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS"
-  }
-};
-
 export const startTimer = () => {
-  axios.post('https://localhost:5001/api/Sleep/start', config)
+  axios.post('https://localhost:5001/api/Sleep/start')
     .then(response => {
       console.log(response.data);
     })
@@ -17,34 +10,21 @@ export const startTimer = () => {
     });
 };
 
-export const pauseTimer = async () => {
-  try {
-    const response = await fetch('https://localhost:5001/api/Sleep/pause', {
-      method: 'POST',
+export const pauseTimer = () => {
+  axios.post('https://localhost:5001/api/Sleep/pause')
+    .then(response => {
+      console.log(response.data);
+    })
+    .catch(error => {
+      console.error('Error while pausing timer:', error);
     });
-
-    if (response.ok) {
-      console.log('Timer paused on the backend');
-    } else {
-      console.error('Failed to pause timer on the backend');
-    }
-  } catch (error) {
-    console.error('Error while pausing timer:', error);
-  }
 };
-
-export const stopTimer = async () => {
-  try {
-    const response = await fetch('https://localhost:5001/api/Sleep/stop', {
-      method: 'POST',
+export const stopTimer = () => {
+  axios.post('https://localhost:5001/api/Sleep/stop')
+    .then(response => {
+      console.log(response.data);
+    })
+    .catch(error => {
+      console.error('Error while stoping timer:', error);
     });
-
-    if (response.ok) {
-      console.log('Timer stopped on the backend');
-    } else {
-      console.error('Failed to stop timer on the backend');
-    }
-  } catch (error) {
-    console.error('Error while stopping timer:', error);
-  }
 };
